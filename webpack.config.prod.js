200786659a0eb0cf011db7efa50e25641202e415
Alexey Bob <alexey.bob@gmail.com>
2020-06-24 19:00:24 +0300

    Add webpack for scss

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -17,6 +17,24 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/i,
+        loader: 'url-loader',
+        options: {
+          limit: 8192,
+        },
+      },
     ],
   },
   resolve: {
